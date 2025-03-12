40342c38ea491586f6e31b3037c8f867838ffaae
gentij <gentijashari5@gmail.com>
2025-03-12 18:15:23 +0100

    Move project to backend folder for submission

--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify";
-import sourceRoutes from "./source.routes";
-
-export default async function routes(fastify: FastifyInstance) {
-  fastify.register(sourceRoutes);
-}
